8c52c80a42e6299f6481f672508ce216444fa057
Chris Kitras <chkitras@gmail.com>
2021-08-03 16:05:00 -0600

    MQTT support added, front end coming soon

--- games/breakout/breakout.py
+++ games/breakout/breakout.py
@@ -3,8 +3,11 @@ sys.path.append('../..')
 
 from lib import seven_seg as ss # import SevenSegment
 from lib import game_display as gd # import Display
+from lib import mqtt_input
+from lib import subscriber as sub
 from time import sleep
-import tty, sys, termios, select
+import tty, sys, termios, select, threading
+import keyboard
 
 SCREEN_Y = 24
 SCREEN_X = 48
@@ -113,6 +116,10 @@ isDown = True
 level = 1
 lives = 3
 
+def update_lives(inc):
+    global lives
+    lives+=inc
+
 def isData():
     return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
 
@@ -141,6 +148,12 @@ def fill_bricks(level=1):
 
 
 def breakout(speed=500):
+
+    input_data = mqtt_input.InputData()
+    data_thread = threading.Thread(target=sub.run, args=([input_data],))
+    data_thread.daemon = True
+    data_thread.start()
+
     global isLeft, isDown, counter, lives
 
     fill_bricks()
@@ -152,97 +165,122 @@ def breakout(speed=500):
     screen.push()
 
 
-    old_settings = termios.tcgetattr(sys.stdin)
-    try:
-        tty.setcbreak(sys.stdin.fileno())
+    # old_settings = termios.tcgetattr(sys.stdin)
+    # try:
+    #     tty.setcbreak(sys.stdin.fileno())
 
-        while True:
+    while True:
 
-            if isData():
-                c = sys.stdin.read(1)
-                
-                if c == "a":
-                    if paddle[0] == 0:
-                        continue
-                    for val in range(len(paddle)):
-                        paddle[val] -= 1
-                    screen.draw_pixel(paddle[0], 23, PIXEL_ON, combine=False)
-                    screen.draw_pixel(paddle[-1] + 1, 23, PIXEL_OFF, combine=False)
+        # if isData():
+        # c = sys.stdin.read(1)
+        # c = input_data.keypress
+        
+        # if c == "a":
+        # if keyboard.is_pressed("a"):
+        #     if paddle[0] == 0:
+        #         continue
+        #     for val in range(len(paddle)):
+        #         paddle[val] -= 1
+        #     screen.draw_pixel(paddle[0], 23, PIXEL_ON, combine=False)
+        #     screen.draw_pixel(paddle[-1] + 1, 23, PIXEL_OFF, combine=False)
 
-                if c == "d":
-                    if paddle[-1] == 47:
-                        continue
-                    for val in range(len(paddle)):
-                        paddle[val] += 1
-                    screen.draw_pixel(paddle[0] - 1, 23, PIXEL_OFF, combine=False)
-                    screen.draw_pixel(paddle[-1], 23, PIXEL_ON, combine=False)
+        # # if c == "d":
+        # if keyboard.is_pressed("d"):
+        #     if paddle[-1] == 47:
+        #         continue
+        #     for val in range(len(paddle)):
+        #         paddle[val] += 1
+        #     screen.draw_pixel(paddle[0] - 1, 23, PIXEL_OFF, combine=False)
+        #     screen.draw_pixel(paddle[-1], 23, PIXEL_ON, combine=False)
 
-                if c == "q":
-                    print("Bye felisha")
-                    break
+        # if c == "q":
+        if keyboard.is_pressed("q"):
+            print("Bye felisha")
+            break
+        
+        counter += 1
+        if counter >= speed:
+
+            counter = 0
+
+            if keyboard.is_pressed("a"):
+                print("left")
+                if paddle[0] == 0:
+                    continue
+                for val in range(len(paddle)):
+                    paddle[val] -= 1
+                screen.draw_pixel(paddle[0], 23, PIXEL_ON, combine=False)
+                screen.draw_pixel(paddle[-1] + 1, 23, PIXEL_OFF, combine=False)
+
+            # if c == "d":
+            if keyboard.is_pressed("d"):
+                if paddle[-1] == 47:
+                    continue
+                for val in range(len(paddle)):
+                    paddle[val] += 1
+                screen.draw_pixel(paddle[0] - 1, 23, PIXEL_OFF, combine=False)
+                screen.draw_pixel(paddle[-1], 23, PIXEL_ON, combine=False)
+
+            if ball[1] <= level + 2:
+                row = ball[1]
+                if row in bricks.keys():
+                    if ball[0] in bricks[row]:
+                        isDown = not isDown
+                        bricks[row].remove(ball[0])
+                        screen.draw_pixel(ball[0], row, PIXEL_OFF)
+                        if not ball[0] % 2:
+                            bricks[row].remove(ball[0] + 1)
+                            screen.draw_pixel(ball[0] + 1, row, PIXEL_OFF)
+                        else:
+                            bricks[row].remove(ball[0] - 1)
+                            screen.draw_pixel(ball[0] - 1, row, PIXEL_OFF)                
             
-            counter += 1
-            if counter >= speed:
+            screen.draw_pixel(ball[0], ball[1], PIXEL_OFF)
 
-                counter = 0
-
-                if ball[1] <= level + 2:
-                    row = ball[1]
-                    if row in bricks.keys():
-                        if ball[0] in bricks[row]:
-                            isDown = not isDown
-                            bricks[row].remove(ball[0])
-                            screen.draw_pixel(ball[0], row, PIXEL_OFF)
-                            if not ball[0] % 2:
-                                bricks[row].remove(ball[0] + 1)
-                                screen.draw_pixel(ball[0] + 1, row, PIXEL_OFF)
-                            else:
-                                bricks[row].remove(ball[0] - 1)
-                                screen.draw_pixel(ball[0] - 1, row, PIXEL_OFF)                
-                
-                screen.draw_pixel(ball[0], ball[1], PIXEL_OFF)
-
-                # Bounds check for ball
-                if ball[0] == 0:
-                    isLeft = False
-                if ball[0] == 47:
-                    isLeft = True
-                if ball[1] == 0:
-                    isDown = True
-                if ball[1] == 22:
-                    if ball[0] in paddle:
-                        isDown = False
-                if ball[1] == 23:
+            # Bounds check for ball
+            if ball[0] == 0:
+                isLeft = False
+            if ball[0] == 47:
+                isLeft = True
+            if ball[1] == 0:
+                isDown = True
+            if ball[1] == 22:
+                if ball[0] in paddle:
                     isDown = False
-                    # lives -= 1
-                    # screen.draw_pixel(ball[0], ball[1], PIXEL_OFF, combine=False)
-                    # if lives == 0:
-                    #     break
-                    # ball[0] = SCREEN_X // 2
-                    # ball[1] = SCREEN_Y // 2
+            if ball[1] == 23:
+                isDown = False
+                # update_lives(-1)
+                # screen.draw_pixel(ball[0], ball[1], PIXEL_OFF, combine=False)
+                # if lives == 0:
+                #     break
+                # ball[0] = SCREEN_X // 2
+                # ball[1] = SCREEN_Y // 2
 
-                if isLeft:
-                    ball[0] -= 1
-                else:
-                    ball[0] += 1
-                if isDown:
-                    ball[1] += 1
-                else:
-                    ball[1] -= 1
+            if isLeft:
+                ball[0] -= 1
+            else:
+                ball[0] += 1
+            if isDown:
+                ball[1] += 1
+            else:
+                ball[1] -= 1
 
-            # Handle the ball
-            screen.draw_pixel(ball[0], ball[1], PIXEL_ON)
+        # Handle the ball
+        screen.draw_pixel(ball[0], ball[1], PIXEL_ON)
 
-            screen.push()
+        screen.push()
 
 
 
-    finally:
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+    # finally:
+    #     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 
 # Test comment
 def main():
+    # while 1:
+    #     if keyboard.is_pressed('q'):
     breakout(500)
+            # print("Banana")
 
 if __name__ == "__main__":
     main()
---
+++ lib/mqtt_input.py
@@ -0,0 +1,27 @@
+import keyboard
+from time import sleep
+
+
+class InputData:
+    def __init__(self):
+        self.keypress = ""
+
+    def update(self, topic, payload):
+        try:
+            topic = topic.split("/")
+            if topic[1] != "input":
+                # print("poo")
+                return
+
+            # input_data = json.loads(payload)
+            # print(input_data)
+            self.keypress = payload
+            # print(self.keypress)
+
+            keyboard.press(self.keypress)
+            sleep(.1)
+            keyboard.release(self.keypress)
+            # print("pressed")
+
+        except Exception as e:
+            print("Something happened", e)
