d2d389e8978dd83f615ce39a725994f649179dff
Philip Lundrigan <philiplundrigan@gmail.com>
2021-08-23 11:57:05 -0600

    Update game of life demo

--- demos/game_of_life.py
+++ demos/game_of_life.py
@@ -1,164 +1,90 @@
+import copy
 import random
-import sys
 from time import sleep
 
-sys.path.append("..")
 
-from lib import seven_seg as ss  # import SevenSegment
-from lib import game_display as gd  # import Display
-
-
-# panel = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=2,
-#     brightness=2,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
-
-# panel2 = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=3,
-#     brightness=2,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
-
-# panel3 = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=4,
-#     brightness=2,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
-
-# panel6 = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=5,
-#     brightness=2,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
-
-# panel5 = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=9,
-#     brightness=2,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
-
-# panel4 = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=10,
-#     brightness=2,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
-# screen = None
-# screen = gd.Display([[panel, panel2, panel3], [panel4, panel5, panel6]], 48, 24)
+def create_board(width, height, density=0.5):
+    return [
+        [int(random.random() < density) for x in range(width)] for y in range(height)
+    ]
 
 
-def init(density=5):
-    for x in range(0, screen.x_width):
-        for y in range(0, screen.y_height):
-            if random.randrange(0, 10) > density:
-                screen.draw_pixel(x, y, 0xF)
-
-    screen.push()
+def display_board(board, screen):
+    for x in range(screen.x_width):
+        for y in range(screen.y_height):
+            screen.draw_pixel(x, y, 0xF if board[y][x] else 0x0)
 
 
-def check_alive(x, y):
-    alive = bool(screen.get_pixel(x, y))
+def print_board(board):
+    for row in board:
+        print(row)
+
+
+def check_alive(x, y, board):
+    alive = board[y][x]
+    board_width = len(board[0])
+    board_height = len(board)
     neighbors_alive = 0
 
     if x > 0:
-        neighbors_alive += bool(screen.get_pixel(x - 1, y))
-    if (x + 1) < screen.x_width:
-        neighbors_alive += bool(screen.get_pixel(x + 1, y))
+        neighbors_alive += board[y][x - 1]
+        # print("Left", board[y][x - 1], neighbors_alive)
+    if (x + 1) < board_width:
+        neighbors_alive += board[y][x + 1]
+        # print("Right", board[y][x + 1], neighbors_alive)
     if y > 0:
-        neighbors_alive += bool(screen.get_pixel(x, y - 1))
-    if (y + 1) < screen.y_height:
-        neighbors_alive += bool(screen.get_pixel(x, y + 1))
+        neighbors_alive += board[y - 1][x]
+        # print("Top", board[y - 1][x], neighbors_alive)
+    if (y + 1) < board_height:
+        neighbors_alive += board[y + 1][x]
+        # print("Bottom", board[y + 1][x], neighbors_alive)
 
     if x > 0 and y > 0:
-        neighbors_alive += bool(screen.get_pixel(x - 1, y - 1))
-    if x > 0 and (y + 1) < screen.y_height:
-        neighbors_alive += bool(screen.get_pixel(x - 1, y + 1))
-    if (x + 1) < screen.x_width and y > 0:
-        neighbors_alive += bool(screen.get_pixel(x + 1, y - 1))
-    if (x + 1) < screen.x_width and (y + 1) < screen.y_height:
-        neighbors_alive += bool(screen.get_pixel(x + 1, y - 1))
+        neighbors_alive += board[y - 1][x - 1]
+        # print("Top-left", board[y - 1][x - 1], neighbors_alive)
+    if x > 0 and (y + 1) < board_height:
+        neighbors_alive += board[y + 1][x - 1]
+        # print("Bottom-left", board[y + 1][x - 1], neighbors_alive)
+    if (x + 1) < board_width and y > 0:
+        neighbors_alive += board[y - 1][x + 1]
+        # print("Top-right", board[y - 1][x + 1], neighbors_alive)
+    if (x + 1) < board_width and (y + 1) < board_height:
+        neighbors_alive += board[y + 1][x + 1]
+        # print("Bottom-right", board[y + 1][x + 1], neighbors_alive)
 
     if alive:
         if neighbors_alive < 2 or neighbors_alive > 3:
-            return False
+            # print("Dead")
+            return 0
         else:
-            return True
+            # print("Alive")
+            return 1
     else:
         if neighbors_alive == 3:
-            return True
+            # print("Alive")
+            return 1
         else:
-            return False
+            # print("Dead")
+            return 0
 
 
-def loop():
-    for x in range(0, screen.x_width):
-        for y in range(0, screen.y_height):
-            alive = check_alive(x, y)
-            if alive:
-                screen.draw_pixel(x, y, 0xF)
-            else:
-                screen.draw_pixel(x, y, 0x0)
-    screen.push()
-    print("Loop")
+def update_board(board):
+    new_board = copy.deepcopy(board)
+    for y in range(len(board)):
+        for x in range(len(board[y])):
+            alive = check_alive(x, y, board)
+            new_board[y][x] = alive
+            # input("-------------------")
+
+    return new_board
 
 
 def game_of_life(display, queue, refresh=10000):
-    global screen
-    screen = display
-    init()
+    board = create_board(display.x_width, display.y_height, density=0.5)
+    print_board(board)
+    display_board(board, display)
 
     while True:
         sleep(1 / refresh)
-        loop()
-
-
-# if __name__ == "__main__":
-#     main()
+        board = update_board(board)
+        display_board(board, display)
