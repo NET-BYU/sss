d961df4623bfd2fdcef9c78f0fe551e838232dd3
apal6981 <ashton.palacios1@gmail.com>
2022-05-04 12:52:27 -0600

    created template demo and updated display file

--- display/display.py
+++ display/display.py
@@ -1,383 +1,52 @@
-from display import seven_seg as ss  # import SevenSegment
-from display import game_display as gd  # import Display
-from loguru import logger
-
-bright = 3
-
-# panel0 = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=12,
-#     brightness=bright,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
-
-# panel1 = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=13,
-#     brightness=bright,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
-
-# panel2 = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=2,
-#     brightness=bright,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
-
-# panel3 = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=3,
-#     brightness=bright,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
-
-# panel4 = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=4,
-#     brightness=bright,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
-
-# panel5 = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=5,
-#     brightness=bright,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
-
-# panel6 = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=6,
-#     brightness=bright,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
-
-# panel7 = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=7,
-#     brightness=bright,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
-
-# panel8 = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=14,
-#     brightness=bright,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
-
-# panel9 = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=9,
-#     brightness=bright,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
-
-# panel10 = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=10,
-#     brightness=bright,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
-
-# panel11 = ss.SevenSegment(
-#     num_digits=96,
-#     cs_num=11,
-#     brightness=bright,
-#     segment_orientation_array=[
-#         [1, 2],
-#         [3, 4],
-#         [5, 6],
-#         [7, 8],
-#         [9, 10],
-#         [11, 12],
-#     ],
-# )
+import seven_seg as ss
+import game_display as gd
 
 
 def create_screen():
-
-    panel0 = ss.SevenSegment(
-        num_digits=96,
-        cs_num=12,
-        brightness=bright,
-        segment_orientation_array=[
-            [1, 2],
-            [3, 4],
-            [5, 6],
-            [7, 8],
-            [9, 10],
-            [11, 12],
-        ],
-    )
-
-    panel1 = ss.SevenSegment(
-        num_digits=96,
-        cs_num=13,
-        brightness=bright,
-        segment_orientation_array=[
-            [1, 2],
-            [3, 4],
-            [5, 6],
-            [7, 8],
-            [9, 10],
-            [11, 12],
-        ],
-    )
-
-    panel2 = ss.SevenSegment(
-        num_digits=96,
-        cs_num=2,
-        brightness=bright,
-        segment_orientation_array=[
-            [1, 2],
-            [3, 4],
-            [5, 6],
-            [7, 8],
-            [9, 10],
-            [11, 12],
-        ],
-    )
-
-    panel3 = ss.SevenSegment(
-        num_digits=96,
-        cs_num=3,
-        brightness=bright,
-        segment_orientation_array=[
-            [1, 2],
-            [3, 4],
-            [5, 6],
-            [7, 8],
-            [9, 10],
-            [11, 12],
-        ],
-    )
-
-    panel4 = ss.SevenSegment(
-        num_digits=96,
-        cs_num=4,
-        brightness=bright,
-        segment_orientation_array=[
-            [1, 2],
-            [3, 4],
-            [5, 6],
-            [7, 8],
-            [9, 10],
-            [11, 12],
-        ],
-    )
-
-    panel5 = ss.SevenSegment(
-        num_digits=96,
-        cs_num=5,
-        brightness=bright,
-        segment_orientation_array=[
-            [1, 2],
-            [3, 4],
-            [5, 6],
-            [7, 8],
-            [9, 10],
-            [11, 12],
-        ],
-    )
-
-    panel6 = ss.SevenSegment(
-        num_digits=96,
-        cs_num=6,
-        brightness=bright,
-        segment_orientation_array=[
-            [1, 2],
-            [3, 4],
-            [5, 6],
-            [7, 8],
-            [9, 10],
-            [11, 12],
-        ],
-    )
-
-    panel7 = ss.SevenSegment(
-        num_digits=96,
-        cs_num=7,
-        brightness=bright,
-        segment_orientation_array=[
-            [1, 2],
-            [3, 4],
-            [5, 6],
-            [7, 8],
-            [9, 10],
-            [11, 12],
-        ],
-    )
-
-    panel8 = ss.SevenSegment(
-        num_digits=96,
-        cs_num=14,
-        brightness=bright,
-        segment_orientation_array=[
-            [1, 2],
-            [3, 4],
-            [5, 6],
-            [7, 8],
-            [9, 10],
-            [11, 12],
-        ],
-    )
-
-    panel9 = ss.SevenSegment(
-        num_digits=96,
-        cs_num=9,
-        brightness=bright,
-        segment_orientation_array=[
-            [1, 2],
-            [3, 4],
-            [5, 6],
-            [7, 8],
-            [9, 10],
-            [11, 12],
-        ],
-    )
-
-    panel10 = ss.SevenSegment(
-        num_digits=96,
-        cs_num=10,
-        brightness=bright,
-        segment_orientation_array=[
-            [1, 2],
-            [3, 4],
-            [5, 6],
-            [7, 8],
-            [9, 10],
-            [11, 12],
-        ],
-    )
-
-    panel11 = ss.SevenSegment(
-        num_digits=96,
-        cs_num=11,
-        brightness=bright,
-        segment_orientation_array=[
-            [1, 2],
-            [3, 4],
-            [5, 6],
-            [7, 8],
-            [9, 10],
-            [11, 12],
-        ],
-    )
-
-    # 5,9,10
-    # 4,11,8
-    # 2,3,7
-    # 0,1,6
-    screen = gd.Display(
+    bright = 3  # brightness of of the panels (1-15)
+    cs_num_lst = [
+        5,
+        10,
+        9,
+        4,
+        11,
+        14,
+        2,
+        3,
+        7,
+        12,
+        13,
+        6,
+    ]  # channel select gpio pin numbers, array is in flattened row order from the top of the screen
+    num_segs_across = 3
+    num_segs_down = 4
+    panel_array = [
         [
-            [panel5, panel10, panel9],
-            [panel4, panel11, panel8],
-            [panel2, panel3, panel7],
-            [panel0, panel1, panel6],
-        ],
+            ss.SevenSegment(
+                num_digits=96,
+                cs_num=cs_num_lst[i * num_segs_across + j],
+                brightness=bright,
+                segment_orientation_array=[
+                    [1, 2],
+                    [3, 4],
+                    [5, 6],
+                    [7, 8],
+                    [9, 10],
+                    [11, 12],
+                ],
+            )
+            for j in range(num_segs_across)
+        ]
+        for i in range(num_segs_down)
+    ]
+    return gd.Display(
+        panel_array,
         48,
         48,
     )
-
-    # screen = gd.Display(
-    #    [[panel9]],
-    #    16,
-    #    12,
-    # )
-
-    return screen
 
 
 def close_screen(screen):
-    logger.info("Closing screen", screen.board_objects)
-    # panel4.close()
-    # panel11.close()
-    # panel8.close()
-    # panel2.close()
-    # panel3.close()
-    # panel7.close()
-    # panel0.close()
-    # panel1.close()
-    # panel6.close()
     for row in range(len(screen.board_objects)):
         for panel in range(len(screen.board_objects[row])):
             screen.board_objects[row][panel].close()
---
+++ template/main.py
@@ -0,0 +1,27 @@
+class Template:
+    """This is a boilerplate class for creating new demos/games for the SSS platform. It needs to include definitions for the following functions: init, run, stop.
+    The init function calls the super init and then intializes the specific demo/game variables.
+    The run function yields a generator. This generator will be called a specified frame rate, this controls what is being pushed to the screen.
+    The stop function is called when the demo/game is being exited by the upper SSS software. It should reset the state for the game"""
+
+    # User input is passed through input_queue
+    # Game output is passed through output_queue
+    # Screen updates are done through the screen object
+    def __init__(self, input_queue, output_queue, screen):
+        # Provide the framerate in frames/seconds and the amount of time of the demo in seconds
+        self.frame_rate = 10
+        self.demo_time = 300  # None for a game
+
+        self.input_queue = input_queue
+        self.output_queue = output_queue
+        self.screen = screen
+        # init demo/game specific variables here
+
+    def run(self):
+        # Create generator here
+        while True:
+            yield
+
+    def stop(self):
+        # Reset the state of the demo if needed, else leave blank
+        pass
