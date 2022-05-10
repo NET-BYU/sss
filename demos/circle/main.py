0cf3df55f912e46d297eab50f9056f348eb60236
Ashton Palacios <ashton.palacios1@gmail.com>
2022-05-10 07:28:33 -0700

    working on converting more demos to template structure

--- demos/circle/main.py
+++ demos/circle/main.py
@@ -1,85 +1,43 @@
-import sys
 
-sys.path.append("..")
+class Circle:
+    """This is a demo that just runs a circle around the perimeter"""
 
-from display import seven_seg as ss  # import SevenSegment
-from display import game_display as gd  # import Display
-import time
+    # User input is passed through input_queue
+    # Game output is passed through output_queue
+    # Screen updates are done through the screen object
+    def __init__(self, input_queue, output_queue, screen):
+        # Provide the framerate in frames/seconds and the amount of time of the demo in seconds
+        self.frame_rate = 30
+        self.demo_time = 30  # None for a game
 
-stop = False
+        self.input_queue = input_queue
+        self.output_queue = output_queue
+        self.screen = screen
+        # init demo/game specific variables here
 
+    def run(self):
+        # Create generator here
+        width = self.screen.x_width
+        height = self.screen.y_height
+        while True:
+            for x in range(1, width):
+                self.screen.draw_pixel(x - 1, 0, 0x0, push=True)
+                self.screen.draw_pixel(x, 0, 0xF, push=True)
+                yield
+            for y in range(1, height):
+                self.screen.draw_pixel(width - 1, y - 1, 0x0, push=True)
+                self.screen.draw_pixel(width - 1, y, 0xF, push=True)
+                yield
+            for x in range(width - 1, 0, -1):
+                self.screen.draw_pixel(x - 1, height - 1, 0xF, combine=False, push=True)
+                self.screen.draw_pixel(x, height - 1, 0x0, combine=False, push=True)
+                yield
+            for y in range(height - 1, 0, -1):
+                self.screen.draw_pixel(0, y - 1, 0xF, combine=False, push=True)
+                self.screen.draw_pixel(0, y, 0x0, combine=False, push=True)
+                yield            
 
-def circle(screen, queue, mqtt_client):
+    def stop(self):
+        # Reset the state of the demo if needed, else leave blank
+        pass
 
-    global stop
-
-    width = screen.x_width
-    height = screen.y_height
-    sleep_time = 0.025
-
-    input_ = ""
-    while input_ != b"q":
-
-        if not queue.empty():
-            input_ = queue.get(block=False)
-        else:
-            input_ = ""
-
-        if input_ == b"q":
-            stop = True
-            break
-
-        for x in range(1, width):
-            if not queue.empty():
-                input_ = queue.get(block=False)
-                if input_ == b"q":
-                    stop = True
-                    break
-            screen.draw_pixel(x - 1, 0, 0x0, push=True)
-            screen.draw_pixel(x, 0, 0xF, push=True)
-            time.sleep(sleep_time)
-
-        if stop:
-            break
-
-        for y in range(1, height):
-            if not queue.empty():
-                input_ = queue.get(block=False)
-                if input_ == b"q":
-                    stop = True
-                    break
-            screen.draw_pixel(width - 1, y - 1, 0x0, push=True)
-            screen.draw_pixel(width - 1, y, 0xF, push=True)
-            time.sleep(sleep_time)
-
-        if stop:
-            break
-
-        for x in range(width - 1, 0, -1):
-            if not queue.empty():
-                input_ = queue.get(block=False)
-                if input_ == b"q":
-                    stop = True
-                    break
-            screen.draw_pixel(x - 1, height - 1, 0xF, combine=False, push=True)
-            screen.draw_pixel(x, height - 1, 0x0, combine=False, push=True)
-            time.sleep(sleep_time)
-
-        if stop:
-            break
-
-        for y in range(height - 1, 0, -1):
-            if not queue.empty():
-                input_ = queue.get(block=False)
-                if input_ == b"q":
-                    stop = True
-                    break
-            screen.draw_pixel(0, y - 1, 0xF, combine=False, push=True)
-            screen.draw_pixel(0, y, 0x0, combine=False, push=True)
-            time.sleep(sleep_time)
-
-        if stop:
-            break
-
-    screen.clear()
-    stop = False
