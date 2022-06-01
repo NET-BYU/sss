611b028f8dc8bbf27cf28d2307ec606e46546f0a
Philip Lundrigan <lundrigan@byu.edu>
2022-06-01 08:46:13 -0700

    Better handling of spidev import

--- display/seven_seg.py
+++ display/seven_seg.py
@@ -1,4 +1,13 @@
-import spidev
+try:
+    import spidev
+except ModuleNotFoundError:
+    print("!" * 80)
+    print("Unable to import spidev.")
+    print("Are you running this on a Raspberry Pi?")
+    print("!" * 80)
+    exit()
+
+
 from display import symbols as sy  # get_char2
 
 MAX7219_DIGITS = 8
