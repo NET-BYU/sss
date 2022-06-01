c5e081fca86f5c55653c2d5313bc92e716eb98c2
Philip Lundrigan <lundrigan@byu.edu>
2022-06-01 08:58:04 -0700

    Use click choice for selecting demo

--- main.py
+++ main.py
@@ -10,22 +10,9 @@ def get_demo_list(demo_dir="demos"):
 
     demos = (d for d in demo_path.iterdir() if d.is_dir())  # Only import directories
     demos = (d for d in demos if (d / "main.py").exists())  # Make sure there is a main
+    demos = [d.name for d in demos]  # Only show name of demo
 
     return demos
-
-
-def existing_demo(demo):
-    demo_list = get_demo_list()
-    demo_list = [d.name for d in demo_list]
-
-    if demo not in demo_list:
-        print("Demo must be one of the following:")
-        for demo in sorted(demo_list):
-            print(f"\t {demo}")
-
-        return False
-
-    return True
 
 
 @click.group()
@@ -45,13 +32,10 @@ def run_kiosk(simulate):
 
 
 @cli.command("demo")
-@click.argument("demo_name", nargs=1)
+@click.argument("name", type=click.Choice(get_demo_list(), case_sensitive=False))
 @click.option("-s", "--simulate", is_flag=True, default=False)
-def run_demo(demo_name, simulate):
-    if not existing_demo(demo_name):
-        return
-
-    demo.run(demo_name, simulate)
+def run_demo(name, simulate):
+    demo.run(name, simulate)
 
 
 if __name__ == "__main__":
