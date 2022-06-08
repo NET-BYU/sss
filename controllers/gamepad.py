b4c557ede875ea8306087bc0df368f7bfc1156a5
Philip Lundrigan <lundrigan@byu.edu>
2022-06-08 15:43:21 -0600

    Re-write game pad handler

--- controllers/gamepad.py
+++ controllers/gamepad.py
@@ -1,35 +1,40 @@
 import threading
+import time
 from queue import Queue, Empty
 
 import inputs
 from loguru import logger
 
 
-def start_processing_input(system_queue, demo_input_queue):
-    # TODO: Handle start and select in system queue?
-    queue = Queue()
-
-    def get_input(queue):
-
+def get_input_from_gamepad(queue):
+    while True:
+        # See if the gamepad is plugged in
         try:
-            _ = inputs.get_gamepad()
-        except inputs.UnpluggedError:
-            logger.warning("There is no controller plugged in")
-            return None
+            gamepad = inputs.devices.gamepads[0]
+        except IndexError:
+            logger.warning("There is no controller plugged in. Trying again later...")
+            time.sleep(5)
+            continue
+
+        # If it is plugged in, get events from it
         while True:
-            events = inputs.get_gamepad()
-            for event in events:
+            for event in gamepad.read():
                 queue.put(event)
 
-    thread = threading.Thread(target=get_input, args=(queue,))
+
+def start_processing_input(system_queue, demo_input_queue):
+    queue = Queue()
+
+    thread = threading.Thread(target=get_input_from_gamepad, args=(queue,), daemon=True)
     thread.start()
 
-    def scan_input():
+    def process():
         leftPressed = False
         rightPressed = False
         upPressed = False
         downPressed = False
 
+        # TODO: Handle start and select in system queue?
         while True:
             while not queue.empty():
                 try:
@@ -70,8 +75,7 @@ def start_processing_input(system_queue, demo_input_queue):
                         demo_input_queue.put("START_P")
                     else:
                         demo_input_queue.put(b"pause")
-                yield
 
             yield
 
-    return scan_input()
+    return process()
