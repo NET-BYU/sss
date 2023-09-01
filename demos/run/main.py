2d8f115110dbf793bfdcbe863a5ad127dcd73b02
emilyk19 <emilyk19@students.byu.edu>
2023-09-01 11:59:59 -0600

    Finished the first iteration of the game

--- demos/run/main.py
+++ demos/run/main.py
@@ -31,6 +31,8 @@ class Run:
         self.is_down = False
         self.is_collision = False
         self.is_cannonball_move = False
+        self.is_reset = False
+        self.game_play = True
 
         # bools for button logic
         # cannonball[0] is the y coordinate of the cannonball cannonball[1] x coordinate
@@ -38,7 +40,7 @@ class Run:
         # dude[0] body y coordinate, dude[1] heady y coordinate, dude[2] x coordinate
         self.dude = [25, 24, 25]
         # This block of code is design set up
-        self.init_screen()
+        # self.init_screen()
         # Draw the character
         self.screen.draw_pixel(self.dude[2], self.dude[1], 0xF, push=True)
         self.screen.draw_pixel(self.dude[2], self.dude[0], 0xF, push=True)
@@ -55,63 +57,131 @@ class Run:
         # Create generator here
         while True:
             self.is_cannonball_move = True
-            # This block moves the character
-            for keypress in get_all_from_queue(self.input_queue):
-                if keypress == "LEFT_P":
-                    self.is_left = True
-                elif keypress == "LEFT_R":
-                    self.is_left = False
-                elif keypress == "RIGHT_P":
-                    self.is_right = True
-                elif keypress == "RIGHT_R":
-                    self.is_right = False
-                elif keypress == "UP_P":
-                    print("Got UP signal")
-                    self.is_jump = True
-                    self.is_up = True
+            if self.game_play:
+                # This block moves the character
+                for keypress in get_all_from_queue(self.input_queue):
+                    if keypress == "LEFT_P":
+                        self.is_left = True
+                    elif keypress == "LEFT_R":
+                        self.is_left = False
+                    elif keypress == "RIGHT_P":
+                        self.is_right = True
+                    elif keypress == "RIGHT_R":
+                        self.is_right = False
+                    elif keypress == "UP_P":
+                        # print("Got UP signal")
+                        self.is_jump = True
+                        self.is_up = True
+                    elif keypress == "START_P":
+                        self.is_reset = True
 
-            self.move()
-            self.collision()
-            self.end_round()
-            # # Set up for cannonballs
-            # def gen_random_speed(self):
-            #     speed = random.randrange(0, 75, 10)
-            #     return speed
+                self.move()
+                self.collision()
 
-            # def gen_rand_time(self):
-            #     time = random.randint(2, 10)
-            #     return time
+                # # Set up for cannonballs
+                # def gen_random_speed(self):
+                #     speed = random.randrange(0, 75, 10)
+                #     return speed
 
-            # set up movement in the loop
+                # def gen_rand_time(self):
+                #     time = random.randint(2, 10)
+                #     return time
 
-            # set up the cannonballs
-            if self.is_cannonball_move == True:
-                if self.cannonball[1] == 0:
-                    self.screen.draw_pixel(self.cannonball[1], 25, 0x0, push=True)
-                    # self.screen.draw_pixel(cannonball[1], 24, 0x0, push=True)
+                # set up movement in the loop
 
-                    self.cannonball[1] = 43
-                    # self.cannonball[0] = random.randint(1, 2)
+                # set up the cannonballs
+                if self.is_cannonball_move == True:
+                    if self.cannonball[1] == 0:
+                        self.screen.draw_pixel(self.cannonball[1], 25, 0x0, push=True)
+                        # self.screen.draw_pixel(cannonball[1], 24, 0x0, push=True)
+                        self.score += 100
+
+                        self.cannonball[1] = 43
+                        # self.cannonball[0] = random.randint(1, 2)
+                    else:
+                        # if self.cannonball[0] == 1:
+                        self.screen.draw_pixel(self.cannonball[1], 25, 0x0, push=True)
+                        self.screen.draw_pixel(
+                            self.cannonball[1] - 1, 25, 0xF, push=True
+                        )
+                        self.cannonball[1] -= 1
                 else:
-                    # if self.cannonball[0] == 1:
                     self.screen.draw_pixel(self.cannonball[1], 25, 0x0, push=True)
-                    self.screen.draw_pixel(self.cannonball[1] - 1, 25, 0xF, push=True)
-                    self.cannonball[1] -= 1
-                    # else:
+                if self.is_collision == True:
+                    self.is_cannonball_move = False
+                    self.screen.clear()
+                    self.screen.draw_text(
+                        (self.screen.x_width // 2) - 5,
+                        26,
+                        "SCORE " + str(self.score),
+                        combine=False,
+                        push=True,
+                    )
+                    self.screen.draw_text(
+                        (self.screen.x_width // 2) - 5, 24, "GAME OVER", push=True
+                    )
+                    self.screen.draw_text(
+                        (self.screen.x_width // 2) - 10,
+                        28,
+                        "PRESS ENTER TO START",
+                        push=True,
+                    )
 
+                    self.game_play = False
+                # print("game_play", self.game_play)
+
+            for reset in get_all_from_queue(self.input_queue):
+                if reset == "START_P":
+                    self.is_reset = True
+
+            # print("reset?", self.is_reset)
+            if self.is_reset:
+                self.screen.draw_text(
+                    (self.screen.x_width // 2) - 5, 24, "         ", push=True
+                )
+                self.screen.draw_text(
+                    (self.screen.x_width // 2) - 5, 26, "          ", push=True
+                )
+                self.screen.draw_text(
+                    (self.screen.x_width // 2) - 10,
+                    28,
+                    "                    ",
+                    push=True,
+                )
+
+                self.score = 0
+                # print("Went into reset")
+
+                # Draw the character
+                self.screen.draw_pixel(self.dude[2], self.dude[1], 0xF, push=True)
+                self.screen.draw_pixel(self.dude[2], self.dude[0], 0xF, push=True)
+
+                # Draw the ground
+                for x_line in range(26, 30):
+                    self.screen.draw_hline(0, x_line, 47, push=True)
+                yield
+
+                # Draws the cannon
+                self.screen.draw_pixel(46, 26, 0xF, push=True)
+                self.screen.draw_pixel(46, 25, 0xF, push=True)
+                self.screen.draw_pixel(45, 25, 0xF, push=True)
+                # Move cannonball back to the beginning
+                self.cannonball[1] = 43
+                # reset the flags
+                self.is_reset = False
+                self.game_play = True
+                self.is_collision = False
+
+                # else:
+                #     self.is_collision = True
             yield
 
-    def end_round(self):
-        if self.is_collision == True:
-            # erases person
-            self.is_cannonball_move = False
-            self.screen.clear()
-            self.screen.draw_text(24, 24, "GAME OVER", push=True)
-            self.is_collision = False
+    # def end_round(self):
 
     def collision(self):
         if self.cannonball[0] == self.dude[0] and self.cannonball[1] == self.dude[2]:
             self.is_collision = True
+            self.is_cannonball_move = False
         else:
             self.is_collision = False
 
