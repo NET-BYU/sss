07557fe832013ac971df537bcd543a0829d7ae6d
Philip Lundrigan <lundrigan@byu.edu>
2025-11-14 20:51:09 -0700

    Use pygame to play a sound

--- broadcasters/sound.py
+++ broadcasters/sound.py
@@ -1,8 +1,7 @@
 from pathlib import Path
-from threading import Thread
 
 from loguru import logger
-from playsound import playsound
+from pygame import mixer
 
 from . import utils
 from .broadcast_message import BroadcastSoundMessage
@@ -18,25 +17,21 @@ def start_processing_output(system_queue, sound_q):
     """
 
     sound_path = Path("sounds")
-
-    def play_sound_background():
-        while True:
-            for item in utils.get_all_from_queue(sound_q):
-                # Filter out things that the sound broadcaster doesn't care about
-                if not isinstance(item, BroadcastSoundMessage):
-                    continue
-
-                logger.debug("Playing sound: {}", item.file)
-
-                sound_file = sound_path / item.file
-                if not sound_file.exists():
-                    logger.error("Sound file does not exist: {}", sound_file)
-                    continue
-
-                playsound(sound_file)
-
-    # Create thread to play sounds
-    Thread(target=play_sound_background, daemon=True).start()
+    mixer.init()
 
     while True:
+        for item in utils.get_all_from_queue(sound_q):
+            # Filter out things that the sound broadcaster doesn't care about
+            if not isinstance(item, BroadcastSoundMessage):
+                continue
+
+            logger.debug("Playing sound: {}", item.file)
+
+            sound_file = sound_path / item.file
+            if not sound_file.exists():
+                logger.error("Sound file does not exist: {}", sound_file)
+                continue
+
+            mixer.Sound(str(sound_file)).play()
+
         yield
